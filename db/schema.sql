46372b09363db89d0bb3a536d4af338cea07a069
Jon Shallcross <jshallcross1@icloud.com>
2021-02-27 11:35:54 -0500

    Create database/ seed database

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employeeDB;
+
+
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE department (
+    department_id INTEGER(10) PRIMARY KEY AUTOINCREMENT,
+    deptName VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+    role_id INTEGER(10) PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10) NOT NULL,
+    department_id INTEGER(10) NOT NULL
+);
+
+CREATE TABLE employee (
+    employee_id INTEGER(10) PRIMARY KEY AUTOINCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER(10) NOT NULL,
+    manager_id INTEGER(10) DEFAULT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,38 @@
+USE employeeDB;
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+("Jon", "Shallcross", 2, null ),
+("Tarek", "Douseki", 1, null ),
+("Mike", "Donohue", 1, null ),
+("Shawn", "Warwick", 2, null ),
+("Tammy", "Wisdom", 2, null ),
+("Max", "Hornell", 3, null ),
+("Curtis", "Taylor", 3, null ),
+("Sean", "Tidevon", 3, null ),
+("Elena", "Gilmour", 4, null ),
+("Madison", "Shallcross", 5, null ),
+("Justice", "Shallcross", 5, null ),
+("Jacksyn", "Hamilton", 2, null ),
+("Ethan", "Shallcross", 3, null ),
+("Dave", "DeJong", 2, null ),
+("Dave", "Hornell", 2, null ),
+
+
+
+
+INSERT INTO empRole (title, salary, department_id)
+VALUES 
+("Manager", 55250.00, 1),
+("Bartender", 72500.50, 2),
+("Cook", 37500.50, 3),
+("Server", 49750.25, 4),
+("Host", 26750.75, 5);
+
+INSERT INTO department (deptName)
+VALUES 
+("Manager"),
+("Bartender"),
+("Cook"),
+("Server"),
+("Host");
